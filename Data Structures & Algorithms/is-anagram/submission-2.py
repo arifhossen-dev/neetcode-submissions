9abb02dcc51aabe539252e80bd6496babd7a2c0d
Arif Hossen <38430220+arifhossen-dev@users.noreply.github.com>
2026-09-01 14:07:43 +0600

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,23 @@
+import string
+# from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if(len(s) != len(t)):
+            return False
+
+        # return Counter(s) == Counter(t)
+
+        alphabet_map_s = {letter: 0 for letter in string.ascii_lowercase}
+        alphabet_map_t = {letter: 0 for letter in string.ascii_lowercase}
+
+
+        for item in s:
+            alphabet_map_s[item] += 1
+
+        for item in t:
+            alphabet_map_t[item] += 1  
+
+        return alphabet_map_s == alphabet_map_t
+        
