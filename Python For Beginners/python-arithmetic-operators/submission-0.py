af90681633be7a97e547b65ac7f1b4c2fed56d5b
Arif Hossen <38430220+arifhossen-dev@users.noreply.github.com>
2026-09-01 16:56:42 +0600

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,16 @@
+a, b, c = 2, 2, 0.5
+
+sum_of_all = a + b + c
+
+zero = 0 - sum_of_all
+
+product = a * b * c 
+
+divid = sum_of_all / product
+
+
+print(sum_of_all)
+print(zero)
+print(product)
+print(divid)
+
