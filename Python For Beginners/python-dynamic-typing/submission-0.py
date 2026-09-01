a909722756ef47e8eb395e4447efce9f9b446f05
Arif Hossen <38430220+arifhossen-dev@users.noreply.github.com>
2026-09-01 16:56:02 +0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,15 @@
+variable = 25
+print(type(variable))
+variable = 98.6
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Alice"
+print(type(variable))
+variable = [1, 2, 3]
+
+
+
+
+
+print(type(variable))
