e9bf9aa23cbf205aaab1166700886005889c77fb
Arif Hossen <38430220+arifhossen-dev@users.noreply.github.com>
2026-09-01 16:56:56 +0600

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,19 @@
+a, b, c = 2, 8, 5
+
+product = a * b
+
+floor_div = product // c
+
+print(floor_div)
+
+remainder = product % c
+
+print(remainder)
+
+rais = a ** b
+
+print(rais)
+
+other_rais = b ** c
+
+print(other_rais)
