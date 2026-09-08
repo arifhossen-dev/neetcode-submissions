e6cdc270fde5c4fc1ba5023dcb5c25e28e7c53b2
Arif Hossen <38430220+arifhossen-dev@users.noreply.github.com>
2026-09-09 00:08:15 +0600

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,14 @@
+def add_two_numbers() -> int:
+    user_input = input()
+
+    sting = user_input.split(",")
+
+    return int(sting[0]) + int(sting[1])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
